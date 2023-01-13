1436ab5a4aea4b8b913357456eb34365148796e5
helina <HELINAAALEMU@gmail.com>
2023-01-13 05:25:32 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x=5;
+        int y=6;
+        if ((x > y)) {
+            System.out.println("x is max");
+        } else {
+            System.out.println("y is max");
+        }
+    }
+}
